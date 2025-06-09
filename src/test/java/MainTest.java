ea6a993dcc2464fb983d5c0f41b557c52afcf260
hyehye <hye1.jeong@samsung.com>
2025-06-09 10:14:16 +0900

    unit test 1차 추가

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,71 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class MainTest {
+
+    @Test
+    void selectStockBrokerKiwer() {
+        AutoTradingSystem system = new AutoTradingSystem();
+        StockBroker broker = new StockBroker(Kiwer);
+        system.selectBroker(broker);
+
+        assertThat(broker).isInstanceOf(KiwerStock.class)
+    }
+
+    @Test
+    void selectStockBrokerKiwer() {
+        AutoTradingSystem system = new AutoTradingSystem();
+        StockBroker broker = new StockBroker(Nemo);
+        system.selectBroker(broker);
+
+        assertThat(broker).isInstanceOf(NemoStock.class)
+    }
+
+
+    @Test
+    void loginKiwerStock() {
+        AutoTradingSystem system = new AutoTradingSystem();
+        StockBroker broker = new StockBroker(Kiwer);
+        system.selectBroker(broker);
+
+        system.login();
+
+        assertThat(system.getLoginInfo()).contains("success");
+    }
+
+    @Test
+    void loginNemoStock() {
+        AutoTradingSystem system = new AutoTradingSystem();
+        StockBroker broker = new StockBroker(Nemo);
+        system.selectBroker(broker);
+
+        system.login();
+
+        assertThat(system.getLoginInfo()).contains("success");
+    }
+
+    @Test
+    void buyByKiwerStock() {
+
+    }
+
+    @Test
+    void buyByNemoStock() {
+
+    }
+
+    @Test
+    void sellByKiwerStock() {
+
+    }
+
+    @Test
+    void sellByNemoStock() {
+
+    }
+
 
 }
